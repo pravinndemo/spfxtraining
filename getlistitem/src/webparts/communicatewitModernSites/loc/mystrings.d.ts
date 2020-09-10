c5b7aa39a713cd6a744431b27038ccf22dafcc0c
pravin080317 <35328578+pravin080317@users.noreply.github.com>
2020-09-10 17:22:43 +0530

    getlisitems

---
+++ getlistitem/src/webparts/communicatewitModernSites/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ICommunicatewitModernSitesWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'CommunicatewitModernSitesWebPartStrings' {
+  const strings: ICommunicatewitModernSitesWebPartStrings;
+  export = strings;
+}
